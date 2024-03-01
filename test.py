e4db6af03e7a3e221d12aa3733aed4ef78198e5f
Harsha Veeravalli <harshaveeravalli@gmail.com>
2024-03-01 22:15:45 +0530

    Update test.py

--- test.py
+++ test.py
@@ -8,9 +8,10 @@ repo_url = 'https://github.com/username/repository_name'
 github_gpt = GitHubGPT(access_token, repo_url)
 github_gpt.read_files()
 
-question = "What is the main function of this code?"
-answer = github_gpt.generate_response(question)
-print(answer)
+while True:
+  question = input("What is your question?\n")
+  answer = github_gpt.generate_response(question)
+  print(answer)
 
 code_suggestion_prompt = "Create a function that takes a list of numbers and returns the sum of all even numbers."
 code_suggestion = github_gpt.generate_response(code_suggestion_prompt, code_suggestion=True)
